6dee24bb99fd859391ca6e83f9560a01fb69b945
MAUAK <kauamsilva360@gmail.com>
2021-10-07 22:09:36 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioHotel/Estudante.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioHotel
+{
+    class Estudante
+    {
+        //Criando e encapsulando uma variavel Nome
+        public string Nome { get; set; }
+        //Criando e encapsulando uma variavel Email
+        public string Email { get; set; }
+
+        //Criando um construtor para o nome e email da classe estudante
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        //retornando o valor da variavel nome e email
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
---
+++ ExercicioHotel/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ExercicioHotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criando um vetor com espaço de 10
+            Estudante[] vect = new Estudante[10];
+
+            //Imprimindo e lendo quantos números de estudantes alugarão os quadros
+            Console.WriteLine("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            //Criando uma estrutura de repetição para declarar os hospedes e suas caracteristicas
+            for (int i = 1; i <= n; i++)
+            {
+                //Pulando a linha
+                Console.WriteLine();
+                //Imprimindo os hospedes na tela com o número da repetição
+                Console.WriteLine($"Hospede #{i}");
+                //Pedindo e lendo o nome do hospede, e guardando na variavel nome da clase Estudante
+                Console.WriteLine("Nome: ");
+                string nome = Console.ReadLine();
+                //Pedindo e lendo o email do hospede, e guardando na variavel nome da clase Estudante
+                Console.WriteLine("Email: ");
+                string email = Console.ReadLine();
+                //Pedindo e lendo o quarto do hospede, e guardando em uma variavel  quarto
+                Console.WriteLine("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                //Criando um vetor com o valor da variavel quarto e guardando junto na classe estudante
+                vect[quarto] = new Estudante(nome, email);
+            }
+
+            //Pulando linha
+            Console.WriteLine();
+            //Imprimindo os quartos ocupados
+            Console.WriteLine("Quartos ocupados: ");
+            //Criando uma repetição para ver quantos quartos estão ocupados
+            for (int i = 0; i < 10; i++)
+            {
+                //comparando se o número da repetição no vetor é diferente de nulo
+                if (vect[i] != null)
+                {
+                    //Escrevendo o número do quarto junto com suas especificações (nome e email)
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}
